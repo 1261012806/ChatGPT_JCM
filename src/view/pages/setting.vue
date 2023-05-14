18f4fdcc4843c66ee4c19a20aece44ca6c6c4e4e
1261012806 <120921222+1261012806@users.noreply.github.com>
2023-05-14 14:02:54 +0800

    Update setting.vue

--- src/view/pages/setting.vue
+++ src/view/pages/setting.vue
@@ -12,19 +12,18 @@
         <transition name="el-zoom-in-top">
           <div v-show="show" class="transition-box">
             <span>
-              现已接入OpenAI的Models API、Completions API、Chat API、Audio API、Images API、Files API、Fine-tunes API后续会添加更多有意思的功能进去，希望大家给我的GitHub点个小小的星星，
-              大家如果有什么好的想法可以在GitHub中提出来，觉得项目不错的朋友可以打赏一波，感谢支持。
+              现已接入OpenAI的Models API、Completions API、Chat API、Audio API、Images API、Files API、Fine-tunes API后续会添加更多有意思的功能满足更多需求。
             </span>
             <div>
-              <a href="https://space.bilibili.com/326625155?spm_id_from=333.337.0.0">BliBili</a>---<a href="https://github.com/202252197/ChatGPT_JCM">GitHub</a>
+              <a href="https://fk.myxinwen.top">OpenAI Key 购买</a>---<a href="https://fk.myxinwen.top">搭建同站点</a>
             </div>
             <div>
-              <h3>愿半生编码，如一生老友</h3>
+              <h3>欢迎使用</h3>
             </div>
-            <div>
+            <!--div>
               <img src="https://i.328888.xyz/2023/05/09/iQ8tvv.jpeg" width="300px" height="300px"/>
               <img src="https://i.328888.xyz/2023/05/09/iQNnf3.jpeg" width="300px" height="300px"/>
-            </div>
+            </div-->
           </div>
         </transition>
       </el-main>
