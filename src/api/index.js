7ff45ec859d9634875ee7066901049b12350819c
1261012806 <120921222+1261012806@users.noreply.github.com>
2023-05-14 23:05:01 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -30,7 +30,7 @@ axios.interceptors.response.use(function (response) {
 
 const base = {
   axios: axios,
-  baseUrl: 'https://api.1ren.ren'
+  baseUrl: 'https://chatai.1rmb.tk'
 }
 
 export default base
